f8119b7f9fb7dec1772d0d9b7b5a10df194123eb
samanta <samantajuste344@gmail.com>
2024-06-23 19:40:19 +0300

    Added mysql database

--- myWorld/tt2FinalProject/tt2FinalProject/settings.py
+++ myWorld/tt2FinalProject/tt2FinalProject/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'tt2FinalProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tt2db',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '',  # Set to an empty string for localhost.
+        'PORT': '',       # Set to an empty string for the default port.
     }
 }
 
