cf0bf3b9e611aa3a97eda0fd9d221ff6b186fbd7
samanta <samantajuste344@gmail.com>
2024-06-23 18:44:08 +0300

    merge everything into django

---
+++ myWorld/tt2FinalProject/login/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from django.template import loader
+
+def login(request):
+  template = loader.get_template('login.html')
+  return HttpResponse(template.render())
+
+def index(request):
+  template = loader.get_template('index.html')
+  return HttpResponse(template.render())
+
+def register(request):
+  template = loader.get_template('register.html')
+  return HttpResponse(template.render())
