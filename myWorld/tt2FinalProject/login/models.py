af7dda7daf97b0603352d92fe2307fce37bf1069
samanta <samantajuste344@gmail.com>
2024-06-23 20:55:48 +0300

    added user registration feature and success window

--- myWorld/tt2FinalProject/login/models.py
+++ myWorld/tt2FinalProject/login/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class Klients (models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.CharField(max_length=30)
+    country = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
--- myWorld/tt2FinalProject/login/views.py
+++ myWorld/tt2FinalProject/login/views.py
@@ -12,3 +12,31 @@ def index(request):
 def register(request):
   template = loader.get_template('register.html')
   return HttpResponse(template.render())
+def success(request):
+  template = loader.get_template('success.html')
+  return HttpResponse(template.render())
+
+# myapp/views.py
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from .forms import UserRegistrationForm
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            User.objects.create_user(username=username, email=email, password=password)
+            messages.success(request, 'User created successfully')
+            return redirect('success')  # Redirect to login or another page
+    else:
+        form = UserRegistrationForm()
+
+    return render(request, 'register.html', {'form': form})
+  
+  
+
